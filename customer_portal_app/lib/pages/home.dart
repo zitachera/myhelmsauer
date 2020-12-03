d1eb3da7ab273fd61d61aa2302ba7e78be37f539
Keller <jan-erik.keller@helmsauer2000.local>
2020-12-03 10:37:20 +0100

    Fixed bottom nav bar color

--- customer_portal_app/lib/pages/home.dart
+++ customer_portal_app/lib/pages/home.dart
@@ -1,6 +1,6 @@
 import 'package:customer_portal_app/components/scaffolds.dart';
-import 'package:customer_portal_app/model/vorgang.dart';
 import 'package:customer_portal_app/model/vertrag.dart';
+import 'package:customer_portal_app/pages/news.dart';
 import 'package:customer_portal_app/pages/vertrag.dart';
 import 'package:flutter/material.dart';
 
@@ -12,50 +12,54 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  List<Vertrag> vertraege = <Vertrag>[
-    Vertrag(
-      id: '<uuid-1>',
-      sparte: 'Kfz-Versicherung',
-      gesellschaft: 'AXA',
-      vertragsnummer: '123',
-      ablauf: DateTime(2029, 11, 3),
-      status: VertragStatus.aktiv,
-      beitrag: '20,00 €',
-      risiko: '??',
-    ),
-    Vertrag(
-      id: '<uuid-2>',
-      sparte: 'Kfz-Versicherung',
-      gesellschaft: 'HDI',
-      vertragsnummer: '456',
-      ablauf: DateTime(2029, 11, 3),
-      status: VertragStatus.aktiv,
-      beitrag: '55,00 €',
-      risiko: 'N XM 333',
-    ),
-  ];
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
 
-  Vertrag vertragZuVorgang(Vorgang vorgang) => vertraege.firstWhere(
-        (v) => v.id == vorgang.vertragsID,
-      );
+  Widget _content = NewsPage();
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+      switch (index) {
+        case 0:
+          _content = NewsPage();
+          break;
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return HsNestedScrollScaffold(
       title: 'Helmsauer',
-      body: Column(
-        children: <Widget>[
-          Text(
-            "Meine Verträge",
-            textScaleFactor: 2,
+      body: _content,
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
           ),
-          ...vertraege.map(
-            (vertrag) => _buildVertrag(
-              context,
-              vertrag,
+          BottomNavigationBarItem(
+            icon: Icon(Icons.text_snippet),
+            label: 'Verträge',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.warning,
+              color: Colors.red,
             ),
+            label: 'Schadenmeldung',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.contact_page),
+            label: 'Kontakt',
           ),
         ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        unselectedItemColor: Colors.grey,
+        onTap: _onItemTapped,
       ),
     );
   }
--- customer_portal_app/lib/pages/vertraege.dart
+++ customer_portal_app/lib/pages/vertraege.dart
@@ -1,6 +1,6 @@
 import 'package:customer_portal_app/components/scaffolds.dart';
+import 'package:customer_portal_app/model/vorgang.dart';
 import 'package:customer_portal_app/model/vertrag.dart';
-import 'package:customer_portal_app/pages/news.dart';
 import 'package:customer_portal_app/pages/vertrag.dart';
 import 'package:flutter/material.dart';
 
@@ -12,53 +12,50 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  static const TextStyle optionStyle =
-      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  List<Vertrag> vertraege = <Vertrag>[
+    Vertrag(
+      id: '<uuid-1>',
+      sparte: 'Kfz-Versicherung',
+      gesellschaft: 'AXA',
+      vertragsnummer: '123',
+      ablauf: DateTime(2029, 11, 3),
+      status: VertragStatus.aktiv,
+      beitrag: '20,00 €',
+      risiko: '??',
+    ),
+    Vertrag(
+      id: '<uuid-2>',
+      sparte: 'Kfz-Versicherung',
+      gesellschaft: 'HDI',
+      vertragsnummer: '456',
+      ablauf: DateTime(2029, 11, 3),
+      status: VertragStatus.aktiv,
+      beitrag: '55,00 €',
+      risiko: 'N XM 333',
+    ),
+  ];
 
-  Widget _content = NewsPage();
-  int _selectedIndex = 0;
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-      switch (index) {
-        case 0:
-          _content = NewsPage();
-          break;
-      }
-    });
-  }
+  Vertrag vertragZuVorgang(Vorgang vorgang) => vertraege.firstWhere(
+        (v) => v.id == vorgang.vertragsID,
+      );
 
   @override
   Widget build(BuildContext context) {
     return HsNestedScrollScaffold(
       title: 'Helmsauer',
-      body: _content,
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
+      body: Column(
+        children: <Widget>[
+          Text(
+            "Meine Verträge",
+            textScaleFactor: 2,
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.text_snippet),
-            label: 'Verträge',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.warning,
-              color: Colors.red,
+          ...vertraege.map(
+            (vertrag) => _buildVertrag(
+              context,
+              vertrag,
             ),
-            label: 'Schadenmeldung',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.contact_page),
-            label: 'Kontakt',
           ),
         ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.amber[800],
-        onTap: _onItemTapped,
       ),
     );
   }
