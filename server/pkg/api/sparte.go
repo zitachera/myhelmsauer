05a37b12aefbf3c14ccb54ebe667416dbc993616
Jan Erik Keller <jan-erik.keller@helmsauer-gruppe.de>
2021-07-19 15:10:15 +0200

    Privathaftpflicht

--- server/pkg/api/sparte.go
+++ server/pkg/api/sparte.go
@@ -117,6 +117,67 @@ func meldeFelderForSparte(sparte string) []meldeFeld {
 				Min:          0,
 			},
 		}
+
+	case "SPN20101222145048KWI", "SPN_304xxxxxxxxxxxxx", "SPN_304_____________", "SPN20080326141601UKQ", "SPN20000803170532V4E": // Haft PHV
+		return []meldeFeld{
+			{
+				Label:        "Kontaktdaten des Geschädigten",
+				Kind:         sectionKind,
+				Beschreibung: "",
+			},
+			{
+				ID:           "vorname",
+				Label:        "Vor\u00ADname",
+				Kind:         textfieldKind,
+				Beschreibung: "",
+			},
+			{
+				ID:           "nachname",
+				Label:        "Nach\u00ADname",
+				Kind:         textfieldKind,
+				Beschreibung: "",
+			},
+			{
+				ID:           "telefonnummer",
+				Label:        "Telefon\u00ADnummer",
+				Kind:         textfieldKind,
+				Beschreibung: "",
+			},
+			{
+				ID:           "e-mail",
+				Label:        "E-Mail",
+				Kind:         textfieldKind,
+				Beschreibung: "",
+			},
+
+			{
+				Label:        "Schaden",
+				Kind:         sectionKind,
+				Beschreibung: "",
+			},
+			{
+				ID:           "schaden",
+				Label:        "Schaden\u00ADbild",
+				Kind:         imagesKind,
+				Beschreibung: "Was ist kaputt gegangen?",
+				Max:          3,
+				Min:          0,
+			},
+			{
+				ID:           "rechnung",
+				Label:        "Anschaffungs- / Reperatur\u00ADrechnung",
+				Kind:         imagesKind,
+				Beschreibung: "Kennzeichen des Unfallgegners",
+				Max:          1,
+				Min:          0,
+			},
+			{
+				ID:           "wert",
+				Label:        "Ungefährer Wert",
+				Kind:         textfieldKind,
+				Beschreibung: "Falls Rechnung nicht vorhanden",
+			},
+		}
 	}
 	return []meldeFeld{}
 }
