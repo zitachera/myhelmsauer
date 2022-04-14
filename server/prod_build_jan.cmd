1d156ec7951bb0a34c2aa9310ae514622860965a
Jan Erik Keller <jan-erik.keller@helmsauer-gruppe.de>
2022-04-14 17:10:57 +0200

    fixed script

--- server/prod_build_jan.cmd
+++ server/prod_build_jan.cmd
@@ -2,4 +2,4 @@ wsl go build
 
  
 scp ./server  mailprox-jump-redmine:schadenmelder
-ssh mailprox-jump-redmine 'sudo /home/jan/adm/update_schadenmelder'
+ssh mailprox-jump-redmine "sudo /home/jan/adm/update_schadenmelder"
