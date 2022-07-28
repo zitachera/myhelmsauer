868fdac7b59ca67a1f84733676b3414941870b8c
Jan Erik Keller <jan-erik.keller@helmsauer-gruppe.de>
2022-07-28 09:00:13 +0200

    Entferne Jade un bbg

--- app/lib/model/firmen_gruppe.dart
+++ app/lib/model/firmen_gruppe.dart
@@ -7,8 +7,6 @@ class FirmenGruppe {
   static final List<FirmenGruppe> all = [
     FirmenGruppe._("hk", "Helmsauer Assekuranzmakler AG"),
     FirmenGruppe._("sue", "Dr. Schmidt & Erdsiek Versicherungsmakler"),
-    FirmenGruppe._("jade", "Dr. Schmidt & Erdsiek (Ex-Jade)"),
-    FirmenGruppe._("bbg", "Dr. Schmidt & Erdsiek (Ex-Berenberg-Gossler)"),
     FirmenGruppe._("aewz", "Ärzte Wirtschaftszentrum Köln"),
     FirmenGruppe._("hp", "Helmsauer und Preuß GmbH"),
     FirmenGruppe._("myh", "myHelmsauer"),
