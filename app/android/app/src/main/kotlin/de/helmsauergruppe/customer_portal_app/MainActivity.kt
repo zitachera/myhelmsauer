22c67faebbff1d17f94e1c88b7336bda8da1f637
Mounyicha <zita.mounyicha@8b6f8330-5840-4879-9157-104ed07ad839.fritz.box>
2025-12-01 22:28:03 +0100

    fix portalservice

--- app/android/app/src/main/kotlin/de/helmsauergruppe/customer_portal_app/MainActivity.kt
+++ app/android/app/src/main/kotlin/de/helmsauergruppe/customer_portal_app/MainActivity.kt
@@ -2,6 +2,4 @@ package de.helmsauergruppe.customer_portal_app
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity() {
-
-}
+class MainActivity : FlutterActivity()
--- app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,7 +10,6 @@ import file_selector_macos
 import flutter_secure_storage_macos
 import geolocator_apple
 import package_info_plus
-import path_provider_foundation
 import pdf_render
 import url_launcher_macos
 import webview_flutter_wkwebview
@@ -21,7 +20,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SwiftPdfRenderPlugin.register(with: registry.registrar(forPlugin: "SwiftPdfRenderPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
